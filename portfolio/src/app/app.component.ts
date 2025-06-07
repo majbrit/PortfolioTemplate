ae7d2209402c365889bce67bd40160ac20461216
Majbrit Schöttner <majbrit.schoettner@yahoo.de>
2025-06-07 21:29:49 +0200

    basis angular

---
+++ portfolio/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CurriculumVitaeComponent } from './components/curriculum-vitae/curriculum-vitae.component';
+import { HomeComponent } from './components/home/home.component';
+import { PublicationsComponent } from './components/publications/publications.component';
+import { ResearchComponent } from './components/research/research.component';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet, 
+    NavigationComponent, 
+    FooterComponent, 
+    ContactComponent, 
+    CurriculumVitaeComponent,
+    HomeComponent,
+    PublicationsComponent,
+    ResearchComponent 
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'portfolio';
+}
+
+
