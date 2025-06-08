a51331aa08414b833754ff7d0f95b9a04f39abf2
Majbrit Schöttner <majbrit.schoettner@yahoo.de>
2025-06-08 11:24:18 +0200

    Navigation

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { PublicationsComponent } from './components/publications/publications.component';
+import { ResearchComponent } from './components/research/research.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent }, 
+    { path: 'publications', component: PublicationsComponent }, 
+    { path: 'research', component: ResearchComponent },
+    { path: 'contact', component: ContactComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' }, 
     { path: '**', redirectTo: '/home' } 
 ];
